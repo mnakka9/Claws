3c53bd41b22e30166d41a013f7db4cbeff363c6e
Teekzilla <47798620+Teekzilla@users.noreply.github.com>
2019-05-01 22:49:06 -0400

    Testing GoGoAnime Resolving

--- src/scrapers/providers/universal/DebugDummy.js
+++ src/scrapers/providers/universal/DebugDummy.js
@@ -12,6 +12,8 @@ module.exports = class DebugDummy extends BaseProvider {
         const resolvePromises = [];
         const debugLinks = [
             // Hardcoded Links
+            'http://play44net.gogoanime.to/new/?w=745&h=450&vid=at/nw/at_adventure_time_season_10_-_11.mp4',
+            'http://play44net.gogoanime.to/embed.php?w=745&h=450&vid=at/nw/at_ben_10_2016_3_-_01.mp4'
         ];
         for (const debugLink of debugLinks) {
             resolvePromises.push(this.resolveLink(debugLink, ws, null, null, '', { isDDL: false}, hasRD));
